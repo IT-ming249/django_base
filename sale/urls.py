d1886d868d78ac78dbd1cf3a6f3972f3a3dac727
何浩明 <14740435+he-haoming-dev@user.noreply.gitee.com>
2025-03-12 15:56:20 +0800

    F/Q

--- sale/urls.py
+++ sale/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path("count",views.count_view, name='sale_count'),
     path("max_min", views.max_min_view, name='sale_max_min'),
     path("sum",views.sum_view, name='sale_sum'),
+    path("f",views.f_view, name='sale_f'),
+    path("q",views.q_view, name='sale_q'),
 ]
--- sale/views.py
+++ sale/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.db.models import Avg, Count, Max, Min, Sum
+from django.db.models import Avg, Count, Max, Min, Sum, F, Q
 from django.shortcuts import render
 
 from sale.models import Book, Author, BookOrder
@@ -10,7 +10,7 @@ from sale.models import Book, Author, BookOrder
 def sale_index(request):
     return HttpResponse("欢迎来到商城主页")
 
-#数据库聚合函数
+##数据库聚合函数
 def avg_view(request):
     #求平均值
     result = Book.objects.aggregate(Avg('price'))
@@ -35,3 +35,16 @@ def sum_view(request):
     print(result.query)
     print(result)
     return HttpResponse("求和")
+
+##F表达式
+def f_view(request):
+    #F('price')表示用price原来的值
+    Book.objects.update(price=F('price')-10)
+    return HttpResponse("F_view")
+
+##Q表达式 并(或)操作
+def q_view(request):
+    books = Book.objects.filter(Q(price__gte=80) | Q(rating__gte=9)).all()
+    for book in books:
+        print(book.name)
+    return HttpResponse("Q_view")
